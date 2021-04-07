3d74c1f9a63d5161a894afb677f0df5c624229bc
Mateus Castro <mateus.fortes@bandtec.com.br>
2021-04-07 09:03:58 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+        <p>
+          Olá mundo, é nois no react.
+        </p>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import Botao from './components/Botao';
+
+
+import './assets/css/style.css'
+import './assets/css/reset.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <NavBar /> 
+    <HomePage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
